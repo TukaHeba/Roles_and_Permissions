eeb5e8a7551704003baa293b1a202ed7c3e52238
Tuka Heba <tuka.heba@gmail.com>
2024-09-27 23:42:27 +0300

    Add CheckPermission Middleware

---
+++ app/Http/Middleware/CheckPermission.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Models\User;
+use App\Services\ApiResponseService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Spatie\FlareClient\Api;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckPermission
+{
+    /**
+     * Handle an incoming request.
+     *
+     * If the user is authenticated and has the required permission go
+     * othorwize return unauthorized response
+     * 
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next, $permission)
+    {
+        $user = User::find(Auth::id());
+
+        if (Auth::check() && $user->hasPermission($permission)) {
+            return $next($request);
+        }
+        return ApiResponseService::error(null, 'Unauthorized', 403);
+    }
+}
